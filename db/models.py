13cd19aab5c6a0d067d58c3934ebec517f9c7f3d
GennadyBr <gennady.budazhapov@gmail.com>
2024-02-02 19:07:54 +0300

    alembic migrations

---
+++ db/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, JSON, CheckConstraint
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column('username', String(36), unique=True, nullable=False)
+    email = Column('email', String(255), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    config = Column(JSON, nullable=False)
+    __table_args__ = (
+        CheckConstraint(
+            'char_length(username) > 5',
+            name='username_min_length'
+        ),
+        CheckConstraint(
+            "regexp_like(email,'^[a-zA-Z][a-zA-Z0-9_\.\-]+@([a-zA-Z0-9-]{2,}\.)+([a-zA-Z]{2,4}|[a-zA-Z]{2}\.[a-zA-Z]{2})$')",
+            name='emailcheck'
+        ),
+    )
